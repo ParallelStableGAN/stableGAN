c61534adcd6aea24c4bee7990731eeb324d737c9
Alex Reustle <areustledev@gmail.com>
2018-11-16 11:45:19 -0500

    Formatting mogdata.py

--- mogdata.py
+++ mogdata.py
@@ -4,11 +4,12 @@ from scipy.spatial import distance
 from scipy.stats import entropy
 
 
-def generate_data_SingleBatch(num_mode=100, radius=24, center=(0, 0), sigma=0.01, batchSize=64):
+def generate_data_SingleBatch(num_mode=100, radius=24, center=(0, 0),
+                              sigma=0.01, batchSize=64):
     num_data_per_class = int(np.ceil(batchSize/num_mode))
     total_data = {}
 
-    t = np.linspace(0, 2*np.pi, num_mode+1)
+    t = np.linspace(0, 2*np.pi, num_mode + 1)
     t = t[:-1]
     x = np.cos(t)*radius + center[0]
     y = np.sin(t)*radius + center[1]
@@ -26,21 +27,21 @@ def generate_data_SingleBatch(num_mode=100, radius=24, center=(0, 0), sigma=0.01
 
 
 def loglikelihood(data, num_mode=100, radius=24, center=(0, 0)):
-    t = np.linspace(0, 2*np.pi, num_mode+1)
+    t = np.linspace(0, 2*np.pi, num_mode + 1)
     t = t[:-1]
     x = np.cos(t)*radius + center[0]
     y = np.sin(t)*radius + center[1]
 
     modes = np.vstack([x, y]).T
-    q = np.ones(num_mode) / num_mode
+    q = np.ones(num_mode)/num_mode
 
     mat = distance.cdist(data, modes)
-    prob = np.bincount(np.argmin(mat, axis=1), minlength=num_mode) / len(data)
+    prob = np.bincount(np.argmin(mat, axis=1), minlength=num_mode)/len(data)
 
     # find the entropy
     try:
-        toReturn =  entropy(q,prob,base=2)
-    except:
+        toReturn = entropy(q, prob, base=2)
+    except BaseException:
         print('Got some Error, return toReturn=-0.1')
         toReturn = -0.1
     return toReturn
