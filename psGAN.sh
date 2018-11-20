461e577fe6ab332a4ce4b5e1c1066ba442d55bbf
Alex Reustle <areustledev@gmail.com>
2018-11-19 20:05:26 -0500

    Example sbatch script

---
+++ psGAN.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#SBATCH -t 00:01:00
+#SBATCH --ntasks=40
+#SBATCH --exclusive
+#SBATCH -p debug
+
+#Number of processes per node to launch (20 for CPU nodes, 2 for GPU nodes)
+NPROC_PER_NODE=20
+
+. ~/.profile
+module load pytorch
+COMMAND="main.py --outf out_predicition --batchSize 128 --niter 1 --distributed --dist_backend=tcp --verbose --lr 0.001 --manualSeed 5206 --pred --nc 1"
+
+HOSTLIST=`scontrol show hostnames $SLURM_JOB_NODELIST`
+MASTER=`/bin/hostname -s`
+
+##Launch the pytorch processes
+RANK=0
+
+for node in $HOSTLIST; do
+  # echo "$node $RANK"
+  ssh -q $node \
+    module load pytorch;
+    pytorch-python3 -m torch.distributed.launch \
+      --nproc_per_node=$NPROC_PER_NODE \
+      --nnodes=$SLURM_JOB_NUM_NODES \
+      --node_rank=$RANK \
+      --master_addr="${MASTER}" \
+      --master_port=1234 \
+      $COMMAND &
+
+  RANK=$((RANK+1))
+done
+wait
